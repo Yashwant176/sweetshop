3f8f0e3d30dac1e30935dc3de3b8b87e408fecfb
Yash <kumarkyashwant@gmail.com>
2025-12-14 15:32:55 +0530

    changes made in api.js as backend deployment url added in .env

--- sweetshop_frontend/sweetshop_frontend/src/api/api.js
+++ sweetshop_frontend/sweetshop_frontend/src/api/api.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/`,
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
